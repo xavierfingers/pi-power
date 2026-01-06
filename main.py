ca350dfb45c2c8f464cd29632e2119daa5fc0a54
xavierfingers <xavierfingers@gmail.com>
2026-01-05 17:27:36 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from mpmath import mp
+def pi(n, p=0):
+        summed = mp.mpf(0)
+        mp.dps = n
+        factorial = 2*mp.sqrt(2)/mp.mpf(9801)
+        for k in range(0, n):
+         t_1 = mp.mpf(mp.factorial(4*k)*mp.mpf(1103 + 26390*k))
+         t_2 = mp.mpf(mp.factorial(k)**4 * mp.mpf(396)**mp.mpf((4*k)))
+         summed += t_1 / t_2
+         inverse_pi = mp.mpf(factorial * summed)
+         p = mp.mpf(1/inverse_pi)
+         print(f"Approximation: {p} digits: {len(str(p))}")
+print("==== CALCULATE PI ====")
+n = int(input("Enter digits: "))
+for i in range(n):
+ pi(i)
